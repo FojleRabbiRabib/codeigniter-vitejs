afb0ca026612643d435eb25bffca09a1ba4f9bfd
FojleRabbiRabib <36526962+FojleRabbiRabib@users.noreply.github.com>
2025-02-07 21:24:12 +0600

    feat: enhance App component styling and update CSS structure

--- src/Frameworks/react-ts/resources/App.tsx
+++ src/Frameworks/react-ts/resources/App.tsx
@@ -5,17 +5,20 @@ import "@/style.css";
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <div className="logos">
-        <img src={reactLogo} className="logo" alt="React logo" />
+    <div className="text-white">
+      <div className="logos flex justify-center">
+        <img src={reactLogo} className="h-24 mx-[10px]" alt="React logo" />
       </div>
       <h2>Codeigniter Vite + React + TypeScript</h2>
-      <p>
-        <button onClick={() => setCount((count) => count + 1)}>
+      <p className="my-6">
+        <button
+          onClick={() => setCount((count) => count + 1)}
+          className="cursor-pointer rounded-[11px] border border-gray-500 px-4 py-2"
+        >
           count is {count}
         </button>
       </p>
-      <p>Edit resources/App.tsx and save to test HMR</p>
+      <p className="my-6">Edit resources/App.tsx and save to test HMR</p>
     </div>
   );
 }
